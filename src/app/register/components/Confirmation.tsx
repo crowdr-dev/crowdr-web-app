42197c8665851281a1f39f4d49edda64a1480efd
eedris <76642653+eedrxs@users.noreply.github.com>
2023-07-21 11:26:13 +0100

    finished confirmation email page

--- src/app/register/components/Confirmation.tsx
+++ src/app/register/components/Confirmation.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Email from "../../../../public/images/email.png"
+
 const Confirmation = () => {
     return (
 <section>
@@ -6,19 +9,16 @@ const Confirmation = () => {
           <h1 className="text-center text-[#0B5351] text-[18px] md:text-[32px] font-[600] mb-[8px] md:mb-[15px]">
             Almost there...
           </h1>
-            <h2 className="text-center text-[#0B5351] text-[14px] md:text-[20px] md:font-[600] mb-[15px]">
+            <h2 className="text-center text-[#0B5351] text-[14px] md:text-[20px] mb-[40px] md:mb-[37px]">
                 Please check your inbox and click the confirmation link to activate your account.
             </h2>
 
           <div className="max-w-[473px]">
-              <div id="fullname" className="flex flex-col mb-[9px]">
-                  <label htmlFor="fullname" className="text-[14px] text-[#344054] mb-[6px]">Full name</label>
-                  <input type="text" id="fullname" name="fullname" placeholder="Enter your full name" className="text-[15px] rounded-lg border border-[#D0D5DD] py-[10px] px-[14px]" />
+              <div id="email_icon" className="flex justify-center items-center md:bg-[#F8F8F8] rounded-full h-[204px] w-[204px] mx-auto md:mb-[60px]">
+                  <Image src={Email} alt="email icon" className="md:w-[90px]" />
               </div>
 
-
-
-              <button className="bg-[#068645] opacity-50 text-white text-[14px] md:text-base font-[400] md:font-[500] leading-[24px] rounded-[10px] w-full py-[12px] px-[20px] mb-[21px]">Resend confirmation email</button>
+              <button className="bg-[#068645] text-white text-[14px] md:text-base font-[400] md:font-[500] leading-[24px] rounded-[10px] w-full py-[12px] px-[20px] mb-[21px]">Resend confirmation email</button>
           </div>
         </div>
       </div>
