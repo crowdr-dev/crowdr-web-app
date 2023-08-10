ca0e5883269bb26bba57d1c96ad44453d3b0b147
eedris <alabialade@gmail.com>
2023-08-10 07:18:44 +0100

    changed folder name from 'components' to 'signup-components'

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import RegisterFormContext from "@/hooks/useRegisterForm";
-import Topbar from "./components/Topbar";
-import FormPages from "./components/FormPages";
+import Topbar from "./signup-components/Topbar";
+import FormPages from "./signup-components/FormPages";
 
 const AboutUs = () => {
   return (
