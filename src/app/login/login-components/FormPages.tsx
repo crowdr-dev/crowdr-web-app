9699b4a9a2d7a89e37c2b7296f4d15b20ffd949c
eedris <alabialade@gmail.com>
2023-08-20 21:22:51 +0100

    quick change

--- src/app/login/login-components/FormPages.tsx
+++ src/app/login/login-components/FormPages.tsx
@@ -1,5 +1,4 @@
 import { useFormContext } from "react-hook-form";
-import { RegisterFormContext } from "@/hooks/useRegisterForm";
 
 import LoginFormContext from '../../../hooks/useLoginForm';
 import ForgotPassword from "./ForgotPassword";
