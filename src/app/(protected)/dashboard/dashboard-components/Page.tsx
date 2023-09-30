33efa3991a656aa187ff50d512514e63b0120c8f
eedris <alabialade@gmail.com>
2023-09-30 22:23:42 +0100

    set up side navbar

--- src/app/(protected)/dashboard/dashboard-components/Page.tsx
+++ src/app/(protected)/dashboard/dashboard-components/Page.tsx
@@ -1,7 +1,7 @@
 import { RFC } from "@/types/Component";
 
 const Page: RFC = ({ children }) => {
-  return <main className="px-[38px] pt-[27px]">{children}</main>;
+  return <main className="pl-[38px] pr-[25px] pt-[27px]">{children}</main>;
 };
 
 export default Page;
