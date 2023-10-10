f681cf1c7951e16d2acb64c2711add5690721299
eedris <alabialade@gmail.com>
2023-10-10 02:33:04 +0100

    setup dashboard to use new icon set

--- src/app/(protected)/(dashboard)/dashboard-components/Sidebar.tsx
+++ src/app/(protected)/(dashboard)/dashboard-components/Sidebar.tsx
@@ -1,28 +1,32 @@
 'use client'
-
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { pageGroups } from "../pages";
+import Icon from "./Icon";
+import Cloud from "../../../../../public/svg/upload-cloud.svg"
 
 const Sidebar = () => {
-  const pathname = usePathname()
+  const currentPath = usePathname()
 
   return (
-    <nav className="flex flex-col border-r-[0.7px] border-[rgba(56, 56, 56, 0.08)] w-[272px] pl-[26px] pr-[27px]">
+    <nav className="flex flex-col overflow-y-auto border-r-[0.7px] border-[rgba(56, 56, 56, 0.08)] w-[272px] pl-[26px] pr-[27px]">
       {pageGroups.map((pageGroup, index) => (
         <div key={index}>
         {index == 0 || <hr className=" border-[rgba(56, 56, 56, 0.08)]" />}
         <div className="py-[27px]">
           {pageGroup.map((page, index) => {
             let pageLinkStyle = "flex items-center font-medium text-[0.96rem] rounded-[0.275rem] pl-[28px] pt-[14px] pb-[15px]"
+            let isCurrentPage = currentPath.startsWith(page.route)
+
             if (index != 0) pageLinkStyle += ' mt-[10px]'
-            if (pathname.startsWith(page.route)) pageLinkStyle += ' text-white bg-[#00B964]'
+            if (isCurrentPage) pageLinkStyle += ' text-white bg-[#00B964]'
 
             return (
               <Link key={index} href={page.route} className={pageLinkStyle}>
-              <Image src={page.iconUrl} width={18} height={18} alt="icon" className="mr-2" />
-              {page.title}
+                <Icon name={page.icon} className="text-inherit text-xl mr-2" />
+                {/* <Image src={Cloud} width={18} height={18} alt="icon" className="mr-2" /> */}
+                {page.title}
             </Link>
             )
           })}
--- src/app/(protected)/(dashboard)/pages.ts
+++ src/app/(protected)/(dashboard)/pages.ts
@@ -1,23 +1,25 @@
-import Icon from "../../../../public/svg/upload-cloud.svg"
+// import Icon from "../../../../public/svg/upload-cloud.svg"
+
+import Icon from "./dashboard-components/Icon";
 
 class Page {
   constructor(
     public route: string,
     public title: string,
-    public iconUrl: string
+    public icon: string
   ) {}
 }
 
 export const pageGroups = [
-  [new Page("/explore", "Discover/Explore", Icon)],
+  [new Page("/explore", "Discover/Explore", 'Heart_01')],
   [
-    new Page("/campaigns", "My Campaigns", Icon),
-    new Page("/donations", "My Donations", Icon),
-    new Page("/manage-webpage", "Manage Website Page", Icon),
+    new Page("/campaigns", "My Campaigns", 'Heart_01'),
+    new Page("/donations", "My Donations", 'Heart_01'),
+    new Page("/manage-webpage", "Manage Website Page", 'Heart_01'),
   ],
   [
-    new Page("/inbox", "Inbox", Icon),
-    new Page("/notifications", "Notifications", Icon),
-    new Page("/settings", "Settings", Icon),
+    new Page("/inbox", "Inbox", 'Heart_01'),
+    new Page("/notifications", "Notifications", 'Heart_01'),
+    new Page("/settings", "Settings", 'Settings'),
   ],
 ];
