e06b231994dc463c1d50faa1b604ea2955fae8ef
eedris <alabialade@gmail.com>
2023-11-09 09:50:07 +0100

    deployment bug fixes

--- src/app/(protected)/(dashboard)/dashboard-components/ExploreCard.tsx
+++ src/app/(protected)/(dashboard)/dashboard-components/ExploreCard.tsx
@@ -65,7 +65,7 @@ const ExploreCard: RFC<ExploreCardProps> = (props) => {
         </div>
       </div>
 
-      <Button text="Donate" className="w-full mt-4 text-center" href={routeTo} />
+      <Button text="Donate" styles={{outer: "w-full mt-4", inner: "!justify-center"}} href={routeTo} />
     </div>
   )
 }
--- src/app/(protected)/(dashboard)/explore/donate-or-volunteer/page.tsx
+++ src/app/(protected)/(dashboard)/explore/donate-or-volunteer/page.tsx
@@ -112,7 +112,7 @@ export default function DonateOrVolunteer() {
             </div>
           </div>
 
-          <Button text="Donate" className="w-full mt-4 text-center" />
+          <Button text="Donate" styles={{outer: "w-full mt-4", inner: "!justify-center"}} />
         </div>
         <div>
           <div>
@@ -202,7 +202,7 @@ export default function DonateOrVolunteer() {
                 </div>
               </div>
 
-              <Button text="Apply" className="w-full mt-4 text-center" />
+              <Button text="Apply" styles={{outer: "w-full mt-4", inner: "!justify-center"}} />
             </div>
           ) : (
             <div className="mt-6">
@@ -258,7 +258,7 @@ export default function DonateOrVolunteer() {
                 </div>
               </div>
 
-              <Button text="Donate" className="w-full mt-4 text-center" />
+              <Button text="Donate" styles={{outer: "w-full mt-4", inner: "!justify-center"}} />
 
               <div className="mt-10">
                 <div className="flex flex-row items-start justify-between">
