0d75c5e14e1f7392e5ab8231a26595609ee1e811
eedris <alabialade@gmail.com>
2023-11-11 14:25:44 +0100

    implemented darkening on hover on Button component

--- src/app/(protected)/(dashboard)/dashboard-components/Button.tsx
+++ src/app/(protected)/(dashboard)/dashboard-components/Button.tsx
@@ -1,15 +1,20 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import { useRef } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import Color from "color"
 
-import { RFC } from "@/types/Component";
-import { CgSpinner } from "react-icons/cg";
+import { RFC } from "@/types/Component"
+import { IconType } from "react-icons/lib"
+
+import { CgSpinner } from "react-icons/cg"
 
 export const Button: RFC<ButtonProps> = ({
   text,
   href,
   onClick,
   iconUrl,
+  icon,
   bgColor,
   textColor,
   outlineColor,
@@ -17,56 +22,75 @@ export const Button: RFC<ButtonProps> = ({
   buttonType,
   loading,
   disabled,
-  styles,
+  className,
 }) => {
-  const props = { text, bgColor, textColor, outlineColor, iconUrl, shadow, loading, styles };
+  const props = {
+    text,
+    bgColor,
+    textColor,
+    outlineColor,
+    iconUrl,
+    icon,
+    shadow,
+    loading,
+  }
+
+  const buttonRef = useRef<any>(null)
+  const buttonClasses =
+    "inline-flex justify-between items-center rounded-lg cursor-pointer text-sm transition px-[16px] py-[10px] " +
+    className
+  const darkerBgColor = darken(bgColor!)
+
+  const buttonStyle: React.CSSProperties = {
+    background: bgColor,
+  }
+  if (outlineColor) buttonStyle.border = `1px solid ${outlineColor}`
+  if (shadow) {
+    buttonStyle.boxShadow = `0px 1px 2px 0px rgba(16, 24, 40, 0.05)`
+    buttonStyle.border = `0.4px solid #D0D5DD`
+  }
 
   return href ? (
-    <Link href={href} className={styles?.outer}>
+    <Link
+      ref={buttonRef}
+      onMouseEnter={() => (buttonRef.current.style.background = darkerBgColor)}
+      onMouseLeave={() => (buttonRef.current.style.background = bgColor!)}
+      href={href}
+      style={buttonStyle}
+      className={buttonClasses}
+    >
       <ButtonContent {...props} />
     </Link>
   ) : (
     <button
+      ref={buttonRef}
+      onMouseEnter={() => (buttonRef.current.style.background = darkerBgColor)}
+      onMouseLeave={() => (buttonRef.current.style.background = bgColor!)}
       type={buttonType}
       aria-label={text}
       onClick={onClick}
       disabled={disabled}
-      className={styles?.outer}
+      style={buttonStyle}
+      className={buttonClasses}
     >
       <ButtonContent {...props} />
     </button>
-  );
-};
+  )
+}
 
 const ButtonContent: RFC<ButtonContentProps> = ({
   text,
-  bgColor,
   textColor,
-  outlineColor,
   iconUrl,
-  shadow,
   loading,
-  styles,
+  ...props
 }) => {
-  const buttonStyle: React.CSSProperties = {
-    background: bgColor,
-  };
   const textStyle: React.CSSProperties = {
-    color: textColor,
-    textAlign: "center"
-  };
-
-  if (outlineColor) buttonStyle.border = `1px solid ${outlineColor}`;
-  if (shadow) {
-    buttonStyle.boxShadow = `0px 1px 2px 0px rgba(16, 24, 40, 0.05)`;
-    buttonStyle.border = `0.4px solid #D0D5DD`;
+    color: textColor || "inherit",
   }
 
   return (
-    <div
-      style={buttonStyle}
-      className={"inline-flex justify-between items-center rounded-lg cursor-pointer text-sm px-[16px] py-[10px] w-full " + styles?.inner}
-    >
+    <>
       {iconUrl && (
         <Image
           src={iconUrl}
@@ -76,18 +100,17 @@ const ButtonContent: RFC<ButtonContentProps> = ({
           className="mr-2"
         />
       )}
+      {props.icon && <props.icon className="mr-2" />}
       <span style={textStyle}>{text}</span>
       {loading && (
-        <span>
-          <CgSpinner
-            size="20px"
-            className="animate-spin icon opacity-100 ml-2.5"
-          />
-        </span>
+        <CgSpinner
+          size="20px"
+          className="animate-spin icon opacity-100 ml-2.5"
+        />
       )}
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export const GrayButton: RFC<ButtonProps> = ({ ...props }) => {
   return (
@@ -97,8 +120,8 @@ export const GrayButton: RFC<ButtonProps> = ({ ...props }) => {
       outlineColor="rgba(230, 230, 230, 0.30)"
       {...props}
     />
-  );
-};
+  )
+}
 
 export const WhiteButton: RFC<ButtonProps> = ({ ...props }) => {
   return (
@@ -108,30 +131,40 @@ export const WhiteButton: RFC<ButtonProps> = ({ ...props }) => {
       outlineColor="rgba(230, 230, 230, 0.30)"
       {...props}
     />
-  );
-};
+  )
+}
 
 // FIXME: DEFAULT PROPS SOON TO BE DEPRECATED
 Button.defaultProps = {
   bgColor: "#00B964",
   textColor: "#FFF",
   buttonType: "button",
-};
+}
 
 type ButtonProps = ButtonContentProps & {
-  href?: string;
-  onClick?: () => void;
-  buttonType?: "button" | "submit" | "reset";
-};
+  href?: string
+  onClick?: () => void
+  buttonType?: "button" | "submit" | "reset"
+}
 
 type ButtonContentProps = {
-  text: string;
-  textColor?: string;
-  bgColor?: string;
-  outlineColor?: string;
-  iconUrl?: string;
-  shadow?: boolean;
+  text: string
+  textColor?: string
+  bgColor?: string
+  outlineColor?: string
+  iconUrl?: string
+  icon?: IconType
+  shadow?: boolean
   loading?: boolean
   disabled?: boolean
-  styles?: {outer?: string, inner?: string}
-};
+  className?: string
+}
+
+function darken(color: string) {
+  const colorObj = Color(color)
+  const brightness = colorObj.luminosity()
+  const darkenedColor =
+    brightness > 0.5 ? colorObj.darken(0.04) : colorObj.darken(0.07)
+
+  return darkenedColor.hex()
+}
