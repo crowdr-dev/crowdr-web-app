2596d306597a0bf288d725c934926702e57f85c5
eedris <alabialade@gmail.com>
2023-11-16 07:38:37 +0100

    set up donations page

--- src/app/(protected)/(dashboard)/dashboard-components/StatCard.tsx
+++ src/app/(protected)/(dashboard)/dashboard-components/StatCard.tsx
@@ -2,26 +2,28 @@ import {FaArrowUp, FaArrowDown} from 'react-icons/fa6'
 import BgPattern from "../../../../../public/assets/stat-card-pattern.png"
 
 import { RFC } from "@/app/common/types/Component";
+import Image from 'next/image';
 
-const StatCard: RFC<StatCardProps> = ({title, figure, percentageChange, changeType, time, pattern, colorScheme}) => {
+const StatCard: RFC<StatCardProps> = ({title, text, percentage, changeType, time, pattern, colorScheme, iconUrl, detail}) => {
   const backgroundColor = colorScheme == 'dark' ? '#00B964' : '#F8F8F8'
   const backgroundImage = pattern ? "url('assets/stat-card-pattern.png')" : '#00B964'
   const changeColor = colorScheme == 'dark' ? 'white' : (changeType == 'rise' ? '#00B964' : '#F36960')
   const titleColor = colorScheme == 'dark' ? 'white' : '#292A2E'
-  const figureColor = colorScheme == 'dark' ? 'white' : '#0C0C0C'
+  const textColor = colorScheme == 'dark' ? 'white' : '#0C0C0C'
 
   return (
-    <div style={{backgroundColor, backgroundBlendMode: 'overlay'}} className="relative rounded-[5px] py-[22.5px] px-[21px]">
+    <div style={{backgroundColor, backgroundBlendMode: 'overlay'}} className="relative flex flex-col justify-center rounded-[5px] py-[22.5px] px-[21px]">
       <p style={{color: titleColor}} className="text-sm mb-2">{title}</p>
-      <p style={{color: figureColor}} className="text-2xl mb-2">{figure}</p>
-      <div className="flex items-center">
+      <p style={{color: textColor}} className="flex text-2xl mb-2">{iconUrl && <Image src={iconUrl} width={30} height={30} alt='icon' />} {text}</p>
+      {percentage && <div className="flex items-center">
         <p className="flex items-center mr-[10px]">
           {changeType == 'rise' && <FaArrowUp style={{fill: changeColor}} className="text-[1.05rem] mr-1" />}
           {changeType == 'fall' && <FaArrowDown style={{fill: changeColor}} className="text-[1.05rem] mr-1" />}
-          <span style={{color: changeColor}}>{percentageChange}%</span>
+          <span style={{color: changeColor}}>{percentage}%</span>
         </p>
         <p className={colorScheme == 'dark' ? "text-white" : 'text-[#5C636E]'}>vs {time}</p>
-      </div>
+      </div>}
+      {detail && <p className="text-xs text-[#667085]">{detail}</p>}
     </div>
   );
 };
@@ -35,10 +37,12 @@ StatCard.defaultProps = {
 
 type StatCardProps = {
   title: string;
-  figure: string;
-  percentageChange: number;
+  text: string;
+  percentage?: number;
   changeType?: "rise" | "fall";
-  time: string;
+  time?: string;
   pattern?: boolean
   colorScheme?: 'light' | 'dark'
+  iconUrl?: string
+  detail?: string | React.ReactNode
 };
--- src/app/(protected)/(dashboard)/donations/page.tsx
+++ src/app/(protected)/(dashboard)/donations/page.tsx
@@ -1,9 +1,149 @@
+"use client"
+import { useEffect, useState } from "react"
+import CampaignCard from "../dashboard-components/CampaignCard"
+import { Button, GrayButton, WhiteButton } from "../dashboard-components/Button"
+import TextInput from "../dashboard-components/TextInput"
+import StatCard from "../dashboard-components/StatCard"
+import { useUser } from "../utils/useUser"
+import { getUser } from "@/app/api/user/getUser"
+import makeRequest from "@/utils/makeRequest"
+import { extractErrorMessage } from "@/utils/extractErrorMessage"
+
+import { Campaign } from "@/app/common/types/Campaign"
+import { BiSearch } from "react-icons/bi"
+import FileDownloadIcon from "../../../../../public/svg/file-download.svg"
+import FilterIcon from "../../../../../public/svg/filter.svg"
+
 const Donations = () => {
+  const [campaigns, setCampaigns] = useState<Campaign[]>([])
+  const user = useUser()
+
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const query = new URLSearchParams({ page: "1", perPage: "10" })
+      const endpoint = `/api/v1/my-campaigns?${query}`
+
+      try {
+        const user = await getUser()
+        const headers = {
+          "Content-Type": "multipart/form-data",
+          "x-auth-token": user?.token!,
+        }
+        const { success, data } = await makeRequest<{
+          success: boolean
+          data: {campaigns: Campaign[]}
+        }>(endpoint, {
+          headers,
+          method: "GET",
+        })
+        setCampaigns(data.campaigns)
+      } catch (error) {
+        const message = extractErrorMessage(error)
+        // toast({ title: "Oops!", body: message, type: "error" })
+      }
+    }
+
+    fetchCampaigns()
+  }, [])
+
   return (
     <div>
-      Donations
+      {/* page title x subtitle */}
+      <hgroup className="mb-[5px]">
+        <h1 className="text-lg md:text-2xl font-semibold text-[#101828] mb-[5px]">
+          My Donations
+        </h1>
+        <p className="text-[15px] text-[#667085]">
+          Manage your donations
+        </p>
+      </hgroup>
+
+      {/* action buttons */}
+      <div className="flex justify-between items-center mb-5 md:mb-10">
+        {/* button group */}
+        <div className="inline-flex rounded-md" role="group">
+          <button
+            type="button"
+            className="px-4 py-[10px] text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          >
+            Custom
+          </button>
+          <button
+            type="button"
+            className="px-4 py-[10px] text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          >
+            7 days
+          </button>
+          <button
+            type="button"
+            className="px-4 py-[10px] text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          >
+            24 hours
+          </button>
+        </div>
+      </div>
+
+      {/* stats */}
+      <div className="grid md:grid-cols-[repeat(3,_minmax(0,_350px))] 2xl:grid-cols-3 gap-4 md:gap-5 mb-[23px] md:mb-[44px]">
+        {/* TODO: get background image */}
+        <StatCard
+          title="Total Donations"
+          text="N235,880.70"
+          pattern
+        />
+        <StatCard
+          title="All Campaigns"
+          text="18"
+          colorScheme="light"
+        />
+        <StatCard
+          title="My Badge"
+          text="Saviour"
+          detail="Keep spreading love to unlock more badges!"
+          colorScheme="light"
+          iconUrl="/svg/badge.png"
+        />
+      </div>
+
+      <div className="flex md:hidden mb-[23px] md:mb-[9px]">
+        <WhiteButton
+          text="Export Report"
+          iconUrl={FileDownloadIcon}
+          shadow
+          className="mr-3"
+        />
+        <Button text="Withdraw Donations" />
+      </div>
+
+      {/* all campaigns x filters */}
+      <h2 className="md:hidden text-lg text-[#292A2E] mb-[9px]">
+        All Campaigns
+      </h2>
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="hidden md:block text-xl text-[#292A2E]">
+          All Campaigns
+        </h2>
+        <TextInput
+          value=""
+          onChange={() => null}
+          placeholder="Search campaigns"
+          icon={BiSearch}
+          styles={{
+            wrapper: "grow mr-[22px] block md:hidden",
+            input: "text-sm",
+          }}
+        />
+        <GrayButton text="Filters" iconUrl={FilterIcon} />
+      </div>
+
+      {/* campaigns */}
+      <div className="grid md:grid-cols-[repeat(2,_minmax(0,_550px))] 2xl:grid-cols-3 gap-x-[10px] gap-y-3 md:gap-y-[40px]">
+        {campaigns.map((campaign) => (
+          <CampaignCard key={campaign._id} campaign={campaign} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default Donations;
+export default Donations
