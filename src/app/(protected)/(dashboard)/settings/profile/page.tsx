9f8bc00dbe03de110f9c9e9139745e01fc81e59a
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-11-18 10:00:11 +0100

    profile cont

--- src/app/(protected)/(dashboard)/settings/profile/page.tsx
+++ src/app/(protected)/(dashboard)/settings/profile/page.tsx
@@ -21,3 +21,31 @@ const Profile = () => {
       setImage(imageUrl);
     }
   }, []);
+
+  const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  }, []);
+
+
+  return (
+    <div className="w-[100%] h-[100vh] pl-[20px] pt-[10px]">
+      <div className=" w-[60%]">
+        <div className=" w-[100%] flex justify-between">
+          <label className="flex flex-col w-[100%] justify-between">
+            <span className="text-[black] font-medium mb-3">Name</span>
+            <div className="flex justify-between w-[100%]">
+              <input
+                type="text"
+                name="name"
+                placeholder="Valerian"
+                className="bg-tertiary py-[13px] w-[48%] px-6 placeholder:text-[#667085] text-[black] rounded-lg outline-none border-[1px] font-medium InputHold"
+              />
+              <input
+                type="text"
+                name="name"
+                placeholder="Pedro"
+                className="bg-tertiary py-[13px] w-[48%] px-6 placeholder:text-[#667085] text-[black] rounded-lg outline-none border-[1px] font-medium InputHold"
+              />
+            </div>
+          </label>
+        </div>
