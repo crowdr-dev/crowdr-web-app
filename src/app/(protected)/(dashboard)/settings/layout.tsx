e0c4534c81d4eb0e5ec2ea89c69c3fddc3bf614a
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-11-20 00:32:39 +0100

    settings linked

--- src/app/(protected)/(dashboard)/settings/layout.tsx
+++ src/app/(protected)/(dashboard)/settings/layout.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Profile from "./profile/page";
 import Password from "./password/page";
 
 const SettingsLayout = () => {
+  const [selectedSetting, setSelectedSetting] = useState("Profile");
+
+  const handleClick = (setting: any) => {
+    setSelectedSetting(setting);
+  };
+
   return (
     <div>
       <h2 className="text-2xl font-bold">Settings</h2>
@@ -11,26 +20,41 @@ const SettingsLayout = () => {
           {" "}
           <Link href="/settings/profile">
             {/* <Profile /> */}
-            Profile
+            <p onClick={() => handleClick("profile")}>Profile</p>
           </Link>
         </div>
         <div className="mr-5">
           <Link href="/settings/password">
             {/* <Password /> */}
-            Passwod
+            <p onClick={() => handleClick("password")}>Password</p>
           </Link>
         </div>
         <div className="mr-5">
-          <Link href="/settings/notifications">Notifications</Link>
+          <Link href="/settings/notifications">
+            <p onClick={() => handleClick("notification")}>Notifications</p>
+          </Link>
         </div>
         <div className="mr-5">
-          <Link href="/settings/payouts">Payment and Payouts</Link>
+          <Link href="/settings/payouts">
+            <p onClick={() => handleClick("payouts")}>Payment and Payouts</p>
+          </Link>
         </div>
         <div className="mr-5">
-          <Link href="/settings/verification">Verification</Link>
+          <Link href="/settings/verification">
+            <p onClick={() => handleClick("verification")}>Verification</p>
+          </Link>
         </div>
       </div>
-      <hr className="border-[green] border-[1px]" />
+
+      <hr className=" border-[rgba(56, 56, 56, 0.08)]" />
+
+      <div>
+        {selectedSetting === "profile" && <Profile />}
+        {selectedSetting === "password" && <Password />}
+        {selectedSetting === "notification" && "Notification"}
+        {selectedSetting === "payouts" && "Payouts"}
+        {selectedSetting === "verification" && "Verification"}
+      </div>
     </div>
   );
 };
