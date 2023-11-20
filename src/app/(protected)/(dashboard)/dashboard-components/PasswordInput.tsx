becd23931fc2da265cf75d3bd2fdaa26266d3099
eedris <alabialade@gmail.com>
2023-11-20 19:16:46 +0100

    added PasswordInput

---
+++ src/app/(protected)/(dashboard)/dashboard-components/PasswordInput.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react"
+
+import { FieldError, UseFormRegisterReturn } from "react-hook-form"
+import { RFC } from "@/app/common/types"
+import { LuEye, LuEyeOff } from "react-icons/lu"
+
+const PasswordInput: RFC<PasswordInputProps> = ({
+  config,
+  label,
+  error,
+  placeholder,
+  optional,
+  ariaLabelledBy,
+  onChange,
+  id,
+  styles,
+  value,
+}) => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (config) {
+      config.onChange(e)
+    }
+
+    if (onChange) {
+      onChange(e)
+    }
+  }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev)
+  }
+
+  return (
+    <span className={styles?.wrapper}>
+      {label && (
+        <label
+          htmlFor={id || config?.name}
+          className="text-[14px] text-[#344054] mb-[6px]"
+        >
+          {label}{" "}
+          {optional && <span className="opacity-[0.44]">(Optional)</span>}
+        </label>
+      )}
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          {...config}
+          id={id || config?.name}
+          value={value}
+          placeholder={placeholder}
+          onChange={handleChange}
+          style={{ boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)" }}
+          aria-labelledby={ariaLabelledBy}
+          className={
+            "text-[13px] rounded-lg border border-[#D0D5DD] w-full py-[10px] pl-[14px] pr-10 " +
+            styles?.input
+          }
+        />
+        <span
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+        >
+          {showPassword ? (
+            <LuEyeOff size="1.25rem" title="Hide password" />
+          ) : (
+            <LuEye size="1.25rem" title="Show password" />
+          )}
+        </span>
+      </div>
+      {error && (
+        <span className="text-[13px] text-[#667085] opacity-[0.67] mt-[6px]">
+          {error?.message}
+        </span>
+      )}
+    </span>
+  )
+}
+
+export default PasswordInput
+
+type PasswordInputProps = {
+  config?: UseFormRegisterReturn
+  label?: string
+  error?: FieldError
+  placeholder?: string
+  optional?: boolean
+  ariaLabelledBy?: string
+  value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  id?: string
+  styles?: {
+    wrapper?: string
+    input?: string
+  }
+}
