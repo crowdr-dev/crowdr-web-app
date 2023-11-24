3d6cce0fc30e6a9da4f29683b1982ca5de52d86c
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-11-24 13:40:49 +0100

    conflict resolved 2

--- src/app/(protected)/(dashboard)/settings/payouts/page.tsx
+++ src/app/(protected)/(dashboard)/settings/payouts/page.tsx
@@ -1,5 +1,37 @@
+import { Buttonprops } from "@/app/common/components/ButtonProps";
+
 const payouts = () => {
-  return <div>payouts</div>;
+  return payouts;
+  // <div>
+  //   <div className="w-[100%] pb-20 pt-[10px]">
+  //     <div className=" w-[60%]">
+  //       <div className="mt-[25px]">
+  //         <span className="text-[black] font-medium mb-3">
+  //           Where is your organisation located
+  //         </span>
+  //       </div>
+
+  //       <div className="w-[100%] flex items-center justify-end mt-[30px]">
+  //         <div className=" w-[90%] flex items-center justify-end gap-[20px]">
+  //           <Buttonprops
+  //             text="Cancel"
+  //             bgColor="bg-white"
+  //             width="w-[100px]"
+  //             color="text-[black]"
+  //             border="border-[1px]"
+  //           />
+  //           <Buttonprops
+  //             text="Save Changes"
+  //             bgColor="bg-[green]"
+  //             width="w-[150px]"
+  //             color="text-[white]"
+  //             border=""
+  //           />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
 };
 
 export default payouts;
