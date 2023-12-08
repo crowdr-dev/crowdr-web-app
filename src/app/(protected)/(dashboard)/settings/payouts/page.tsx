8237497e19aca4d6bb47d4fd3883cda13acde78b
Damilola Fortune Sanni <nicsylvia15f@gmail.com>
2023-12-08 16:34:45 +0100

    Touch up to be done later

--- src/app/(protected)/(dashboard)/settings/payouts/page.tsx
+++ src/app/(protected)/(dashboard)/settings/payouts/page.tsx
@@ -18,35 +18,49 @@ const Payouts: RFC<BankFormProps> = ({ BankId }) => {
   const isEdit = Boolean(BankId);
   const saveButtonText = isEdit ? "Save Changes" : "Add Bank Details";
 
+  const [show, setShow] = useState(false);
+
+  const HandleShow = () => {
+    setShow(true);
+  };
+
   return (
     <div>
       <div className="w-[100%] pb-20 pt-[10px]">
-        <div className="leading-[25px] text-[16px] cursor-pointer">
-          <p>
-            You have no connected bank account yet.{" "}
-            <span className="text-[#FF5200]">Click here to add</span>
-          </p>
-        </div>
+        {show ? null : (
+          <>
+            <div className="leading-[25px] text-[16px] cursor-pointer">
+              <p>
+                You have no connected bank account yet.{" "}
+                <span onClick={HandleShow} className="text-[#FF5200]">
+                  Click here to add
+                </span>
+              </p>
+            </div>
+          </>
+        )}
 
-        <div className="flex w-[50%] flex-col  mb-6">
-          <h2>Account Number</h2>
-          <TextInput
-            value={input}
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-            placeholder="2267350360"
-            // icon={BiSearch}
-            styles={{
-              wrapper: "grow mr-[22px] block",
-              input: "text-sm",
-            }}
-          />
-        </div>
+        {!show ? null : (
+          <>
+            <div className="flex w-[50%] flex-col  mb-6">
+              <h2>Account Number</h2>
+              <TextInput
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+                placeholder="2267350360"
+                // icon={BiSearch}
+                styles={{
+                  wrapper: "grow mr-[22px] block",
+                  input: "text-sm",
+                }}
+              />
+            </div>
 
-        <div className="flex w-[50%] flex-col  mb-6">
-          <h2>Bank</h2>
-          {/* <SelectInput
+            <div className="flex w-[50%] flex-col  mb-6">
+              <h2>Bank</h2>
+              {/* <SelectInput
             name="BankType"
             options={BankTypes}
             rules={{
@@ -54,49 +68,51 @@ const Payouts: RFC<BankFormProps> = ({ BankId }) => {
             }}
             ariaLabel="Bank Type"
           /> */}
-        </div>
-
-        <div className="flex w-[50%] flex-col  mb-6">
-          <h2>Account Name</h2>
-          <TextInput
-            value={input}
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
-            placeholder="This should auto generate"
-            // icon={BiSearch}
-            styles={{
-              wrapper: "grow mr-[22px] block",
-              input: "text-sm",
-            }}
-          />
-        </div>
-
-        <div className="flex md:justify-end justify-center items-center w-[50%] mb-5">
-          <div className="">
-            <WhiteButton text="Cancel" href="/" shadow className="mr-3" />
-          </div>
-          <div className="flex items-center justify-center ">
-            <Button text={saveButtonText} />
-          </div>
-        </div>
-
-        <div className="flex w-[512px] h-[85px] items-center bg-[#F9FAFB] justify-between p-4">
-          <div className="flex flex-col justify-between">
-            <div className="text-black font-PolySans text-sm font-normal">
-              Guaranty Trust Bank
             </div>
-            <div className="text-gray-500 font-PolySans text-sm font-normal leading-5">
-              210805002
+
+            <div className="flex w-[50%] flex-col  mb-6">
+              <h2>Account Name</h2>
+              <TextInput
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+                placeholder="This should auto generate"
+                // icon={BiSearch}
+                styles={{
+                  wrapper: "grow mr-[22px] block",
+                  input: "text-sm",
+                }}
+              />
             </div>
-            <div className="text-gray-500 font-PolySans text-sm font-normal leading-5">
-              Crowdr Foundation
+
+            <div className="flex md:justify-end justify-center items-center w-[50%] mb-5">
+              <div className="">
+                <WhiteButton text="Cancel" href="/" shadow className="mr-3" />
+              </div>
+              <div className="flex items-center justify-center ">
+                <Button text={saveButtonText} />
+              </div>
             </div>
-          </div>
-          <div className="text-orange-500 font-PolySans text-sm font-normal leading-5 underline cursor-pointer">
-            Edit details
-          </div>
-        </div>
+
+            <div className="flex w-[512px] h-[85px] items-center bg-[#F9FAFB] justify-between p-4">
+              <div className="flex flex-col justify-between">
+                <div className="text-black font-PolySans text-sm font-normal">
+                  Guaranty Trust Bank
+                </div>
+                <div className="text-gray-500 font-PolySans text-sm font-normal leading-5">
+                  210805002
+                </div>
+                <div className="text-gray-500 font-PolySans text-sm font-normal leading-5">
+                  Crowdr Foundation
+                </div>
+              </div>
+              <div className="text-orange-500 font-PolySans text-sm font-normal leading-5 underline cursor-pointer">
+                Edit details
+              </div>
+            </div>
+          </>
+        )}
 
         {/* Table */}
         <Tabs.Item heading="Reference">
