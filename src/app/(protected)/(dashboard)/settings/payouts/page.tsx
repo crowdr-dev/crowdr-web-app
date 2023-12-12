4cc512000f82ce50e2a3c014913defa2f4cb4750
eedris <alabialade@gmail.com>
2023-12-12 09:14:42 +0100

    quick change

--- src/app/(protected)/(dashboard)/settings/payouts/page.tsx
+++ src/app/(protected)/(dashboard)/settings/payouts/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-import Tabs from "../../dashboard-components/Tabs";
-import Table from "../../dashboard-components/Table";
-import Label from "../../dashboard-components/Label";
-import Detail from "../../dashboard-components/Detail";
+"use client"
+import Tabs from "../../dashboard-components/Tabs"
+import Table from "../../dashboard-components/Table"
+import Label from "../../dashboard-components/Label"
+import Detail from "../../dashboard-components/Detail"
 
 const payouts = () => {
   return (
@@ -16,79 +16,77 @@ const payouts = () => {
         </div>
 
         {/* Table */}
-        <Tabs.Item heading="Reference">
-          <Table className="hidden md:block mb-9">
-            <Table.Head>
-              <Table.HeadCell>Refernce No</Table.HeadCell>
-              <Table.HeadCell>Amount</Table.HeadCell>
-              <Table.HeadCell>Date & time</Table.HeadCell>
-              <Table.HeadCell>Status</Table.HeadCell>
-            </Table.Head>
+        <Table className="hidden md:block mb-9">
+          <Table.Head>
+            <Table.HeadCell>Refernce No</Table.HeadCell>
+            <Table.HeadCell>Amount</Table.HeadCell>
+            <Table.HeadCell>Date & time</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+          </Table.Head>
 
-            <Table.Body>
-              {reference.map((donation, index) => (
-                <Table.Row key={index}>
-                  <Table.Cell>{donation.refernceno}</Table.Cell>
-                  <Table.Cell>{donation.amount}</Table.Cell>
-                  <Table.Cell>{donation.date}</Table.Cell>
-                  <Table.Cell>
-                    {donation.status.match(/success/i) ? (
-                      <Label text={donation.status} />
-                    ) : (
-                      <Label
-                        text={donation.status}
-                        textColor="#B42318"
-                        bgColor="#FEF3F2"
-                      />
-                    )}
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-
-          <div className="flex flex-col md:hidden">
+          <Table.Body>
             {reference.map((donation, index) => (
-              <Detail key={index} {...donation} />
+              <Table.Row key={index}>
+                <Table.Cell>{donation.title}</Table.Cell>
+                <Table.Cell>{donation.detail}</Table.Cell>
+                <Table.Cell>{donation.date}</Table.Cell>
+                <Table.Cell>
+                  {donation.status.match(/success/i) ? (
+                    <Label text={donation.status} />
+                  ) : (
+                    <Label
+                      text={donation.status}
+                      textColor="#B42318"
+                      bgColor="#FEF3F2"
+                    />
+                  )}
+                </Table.Cell>
+              </Table.Row>
             ))}
-          </div>
-        </Tabs.Item>
+          </Table.Body>
+        </Table>
+
+        <div className="flex flex-col md:hidden">
+          {reference.map((donation, index) => (
+            <Detail key={index} {...donation} />
+          ))}
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default payouts;
+export default payouts
 
 const reference = [
   {
-    refernceno: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
-    amount: "N40,000.00",
+    title: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
+    detail: "N40,000.00",
     date: "Tue 26 Jul, 2022; 10:14 PM",
     status: "Success",
   },
   {
-    refernceno: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
-    amount: "N40,000.00",
+    title: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
+    detail: "N40,000.00",
     date: "Tue 26 Jul, 2022; 10:14 PM",
     status: "Failed",
   },
   {
-    refernceno: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
-    amount: "N21,300.00",
+    title: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
+    detail: "N21,300.00",
     date: "Tue 26 Jul, 2022; 10:14 PM",
     status: "Success",
   },
   {
-    refernceno: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
-    amount: "N21,300.00",
+    title: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
+    detail: "N21,300.00",
     date: "Tue 26 Jul, 2022; 10:14 PM",
     status: "Success",
   },
   {
-    refernceno: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
-    amount: "N21,300.00",
+    title: "crowdr_test_bank_withdraw_PpHpXxz3uLMvj8Pi",
+    detail: "N21,300.00",
     date: "Tue 26 Jul, 2022; 10:14 PM",
     status: "Failed",
   },
-];
+]
