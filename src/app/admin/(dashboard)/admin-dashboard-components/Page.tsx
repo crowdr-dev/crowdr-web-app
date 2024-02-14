9fc09db0540b5de2fe5eb920d5f90479a050efe9
eedris <alabialade@gmail.com>
2024-02-14 08:53:15 +0100

    moved sidebar into admin-dashboard/dashboard/layout.tsx

--- src/app/admin/(dashboard)/admin-dashboard-components/Page.tsx
+++ src/app/admin/(dashboard)/admin-dashboard-components/Page.tsx
@@ -9,7 +9,7 @@ const Page: RFC = ({ children }) => {
   }
 
   return (
-    <main className="grow overflow-y-auto overflow-x-hidden border-l-[0.7px] border-[rgba(56, 56, 56, 0.08)] px-5 md:px-8 pt-[9px] md:pt-[27px] pb-[27px]">
+    <main className="grow overflow-y-auto overflow-x-hidden border-l-[0.7px] border-[rgba(56, 56, 56, 0.08)] px-6 py-10">
       {children}
     </main>
   )
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,7 @@
 import { PropsWithChildren } from "react"
-import Sidebar from "./(dashboard)/admin-dashboard-components/Sidebar"
-import Page from "./(dashboard)/admin-dashboard-components/Page"
 
 const AdminLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="flex h-full overflow-hidden">
-      <Sidebar />
-      <Page children={children} />
-    </div>
-  )
+  return <div className="h-full">{children}</div>
 }
 
 export default AdminLayout
