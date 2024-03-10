0caa3b4a2044032129084d5d37bfdb2ceb4f5259
eedris <alabialade@gmail.com>
2024-03-10 06:18:11 +0100

    added withdrawal modal

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react"
 import Modal from "../common/components/Modal"
 import SidebarModal from "../(protected)/(dashboard)/dashboard-components/SidebarModal"
 import KycPopup from "./(dashboard)/admin-dashboard-components/KycPopup"
+import WithdrawalPopup from "./(dashboard)/admin-dashboard-components/WithdrawalPopup"
 
 const AdminLayout = ({ children }: PropsWithChildren) => {
   return (
@@ -12,6 +13,10 @@ const AdminLayout = ({ children }: PropsWithChildren) => {
 
           <SidebarModal id="kycPopup" position="center">
             <KycPopup />
+          </SidebarModal>
+
+          <SidebarModal id="withdrawalPopup" position="center">
+            <WithdrawalPopup />
           </SidebarModal>
         </>
       }
