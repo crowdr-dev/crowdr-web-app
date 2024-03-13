64dd8f8fc46224b92be9a9652912970c425ed607
jerry <profkiti@gmail.com>
2024-03-13 10:20:12 +0000

    setup notifications

--- src/app/(protected)/(dashboard)/layout.tsx
+++ src/app/(protected)/(dashboard)/layout.tsx
@@ -1,26 +1,29 @@
-"use client"
-import Header from "./dashboard-components/Header"
-import Sidebar from "./dashboard-components/Sidebar"
-import Page from "./dashboard-components/Page"
-import UserProvider from "./common/hooks/useUser"
+"use client";
+import Header from "./dashboard-components/Header";
+import Sidebar from "./dashboard-components/Sidebar";
+import Page from "./dashboard-components/Page";
+import UserProvider from "./common/hooks/useUser";
 
-import { RFC } from "@/app/common/types"
-import { PropsWithChildren } from "react"
-import "react-loading-skeleton/dist/skeleton.css"
+import { RFC } from "@/app/common/types";
+import { PropsWithChildren } from "react";
+import "react-loading-skeleton/dist/skeleton.css";
+import { NotificationProvider } from "./common/hooks/useNotification";
 
 const DashboardLayout: RFC = (props: PropsWithChildren) => {
   return (
     <UserProvider>
-      <div className="flex flex-col h-full">
-        <Header />
-        <div className="flex grow overflow-hidden">
-          <Sidebar />
-          <Page {...props} />
-          {/* <Page children={children} /> won't work */}
+      <NotificationProvider>
+        <div className="flex flex-col h-full">
+          <Header />
+          <div className="flex grow overflow-hidden">
+            <Sidebar />
+            <Page {...props} />
+            {/* <Page children={children} /> won't work */}
+          </div>
         </div>
-      </div>
+      </NotificationProvider>
     </UserProvider>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
