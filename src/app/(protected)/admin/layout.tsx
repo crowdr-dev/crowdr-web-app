d555235b8e7f72d33ad1ec4fed631eb3d184a253
kachi23-web <onyekanwaks@gmail.com>
2024-04-06 13:21:47 +0100

    kyc and withdrawals

--- src/app/(protected)/admin/layout.tsx
+++ src/app/(protected)/admin/layout.tsx
@@ -1,27 +1,31 @@
-import { PropsWithChildren } from "react"
-import Modal from "../../common/components/Modal"
-import SidebarModal from "../(dashboard)/dashboard-components/SidebarModal"
-import KycPopup from "./admin-dashboard-components/KycPopup"
-import WithdrawalPopup from "./admin-dashboard-components/WithdrawalPopup"
+"use client";
+import { PropsWithChildren } from "react";
+import Modal from "../../common/components/Modal";
+import SidebarModal from "../(dashboard)/dashboard-components/SidebarModal";
+import KycPopup from "./admin-dashboard-components/KycPopup";
+import WithdrawalPopup from "./admin-dashboard-components/WithdrawalPopup";
+import UserProvider from "../(dashboard)/common/hooks/useUser";
 
 const AdminLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-full">
-      {
-        <>
-          {children}
+    <UserProvider>
+      <div className="h-full">
+        {
+          <>
+            {children}
 
-          <SidebarModal id="kycPopup" position="center">
-            <KycPopup />
-          </SidebarModal>
+            <SidebarModal id="kycPopup" position="center">
+              <KycPopup />
+            </SidebarModal>
 
-          <SidebarModal id="withdrawalPopup" position="center">
-            <WithdrawalPopup />
-          </SidebarModal>
-        </>
-      }
-    </div>
-  )
-}
+            <SidebarModal id="withdrawalPopup" position="center">
+              <WithdrawalPopup />
+            </SidebarModal>
+          </>
+        }
+      </div>
+    </UserProvider>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
