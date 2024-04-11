6ed7693b1512075668022213678efb648b481bef
eedris <technocr.aterpdevelopers@gmail.com>
2024-04-11 11:14:29 +0100

    fixed breaking in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-'use client' // TODO: TRY TO RETURN TO SERVER COMPONENT; LOOK AT ModalProvider
-import { useRouter } from "next/router";
+"use client" // TODO: TRY TO RETURN TO SERVER COMPONENT; LOOK AT ModalProvider
+import { useRouter } from "next/router"
 import { PropsWithChildren, useEffect } from "react"
 import Head from "next/head"
+import { usePathname } from "next/navigation"
 import { Public_Sans, Lato } from "next/font/google"
-import localFont from 'next/font/local'
-import {QueryClient, QueryClientProvider} from 'react-query'
+import localFont from "next/font/local"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { Toaster } from "react-hot-toast"
 import ModalProvider from "./common/hooks/useModal"
 import "./globals.css"
 import "./common/styles/button.css"
-import { Metadata } from 'next';
+import { Metadata } from "next"
 import NewLogo from "../../public/svg/new-crowdr-logo.svg"
 
 export const lato = Lato({
@@ -18,24 +19,22 @@ export const lato = Lato({
   subsets: ["latin"],
 })
 
-
-
 const satoshi = localFont({
   src: [
     {
-      path: '../../public/fonts/Satoshi-Regular.otf',
-      weight: '400'
+      path: "../../public/fonts/Satoshi-Regular.otf",
+      weight: "400",
     },
     {
-      path: '../../public/fonts/Satoshi-Medium.otf',
-      weight: '500'
+      path: "../../public/fonts/Satoshi-Medium.otf",
+      weight: "500",
     },
     {
-      path: '../../public/fonts/Satoshi-Bold.otf',
-      weight: '700'
-    }
+      path: "../../public/fonts/Satoshi-Bold.otf",
+      weight: "700",
+    },
   ],
-  variable: '--font-satoshi'
+  variable: "--font-satoshi",
 })
 
 const inter = Public_Sans({ subsets: ["latin"] })
@@ -43,23 +42,29 @@ const inter = Public_Sans({ subsets: ["latin"] })
 const queryClient = new QueryClient()
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  const pathname = window ? window.location.pathname : '';
+  const pathname = usePathname()
 
   useEffect(() => {
     if (pathname) {
-      document.title = pathname.length > 1  ? `${(pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2) + " | ") } Crowdr — Crowdfund in Nigeria` : "Crowdr — Crowdfund in Nigeria";
-      const metaDescription = `Explore campaigns and spread love by donating or volunteering`;
-      const metaTag = document.querySelector('meta[name="description"]');
+      document.title =
+        pathname.length > 1
+          ? `${
+              pathname.slice(1).charAt(0).toUpperCase() +
+              pathname.slice(2) +
+              " | "
+            } Crowdr — Crowdfund in Nigeria`
+          : "Crowdr — Crowdfund in Nigeria"
+      const metaDescription = `Explore campaigns and spread love by donating or volunteering`
+      const metaTag = document.querySelector('meta[name="description"]')
       if (metaTag) {
-        metaTag.setAttribute('content', metaDescription);
+        metaTag.setAttribute("content", metaDescription)
       }
-      const ogImageTag = document.querySelector('meta[property="og:image"]');
+      const ogImageTag = document.querySelector('meta[property="og:image"]')
       if (ogImageTag) {
-        ogImageTag.setAttribute('content', NewLogo);
+        ogImageTag.setAttribute("content", NewLogo)
       }
     }
   }, [pathname])
-
 
   return (
     <>
@@ -67,9 +72,12 @@ export default function RootLayout({ children }: PropsWithChildren) {
         <title>Crowdr</title>
         <meta name="description" content={"Build Community through Giving"} />
         <meta property="og:image" content={NewLogo} />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"
+          rel="stylesheet"
+        />
       </Head>
-      
+
       <body className={`${satoshi.variable} ${inter.className}`}>
         <QueryClientProvider client={queryClient}>
           <ModalProvider>
