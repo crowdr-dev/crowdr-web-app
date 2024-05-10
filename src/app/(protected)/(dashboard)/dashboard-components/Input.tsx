826764d76fbd617c6582bbd907683642d068d356
Daniel Arikawe <arikawedaniel@gmail.com>
2024-05-10 09:53:59 +0100

    pricing update

--- src/app/(protected)/(dashboard)/dashboard-components/Input.tsx
+++ src/app/(protected)/(dashboard)/dashboard-components/Input.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { RFC } from '@/app/common/types'
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
 
 import { useFormContext } from 'react-hook-form'
 
@@ -11,15 +13,27 @@ type InputProps = {
   value?: string | number
   type?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  info?: string
 }
 
 const Input: RFC<InputProps> = props => {
-  const { label, placeholder, name, id , value, onChange, type} = props
+  const { label, placeholder, name, id , value, onChange, type,info} = props
 
   return (
     <div className='flex flex-col mb-[14px]'>
-      <label htmlFor={id} className='text-[14px] text-[#344054] mb-[6px]'>
+      <label htmlFor={id} className='text-[14px] text-[#344054] mb-[6px] flex flex-row items-center gap-1'>
         {label}
+       {
+        info && <>
+        <a data-tooltip-id="my-tooltip" data-tooltip-content={info}>
+  <Image 
+  src={'/svg/info.svg'}
+  height={15}
+  width={15}
+  alt="info"/>
+</a>
+<Tooltip id="my-tooltip" /></>
+       } 
       </label>
 
       <input
