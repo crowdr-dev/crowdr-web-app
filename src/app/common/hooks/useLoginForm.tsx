2079c9923aab69d1b503198be6c5a9c99107cf68
Daniel Arikawe <arikawedaniel@gmail.com>
2025-05-02 09:51:33 +0100

    feat: fixed client side error for cookies

--- src/app/common/hooks/useLoginForm.tsx
+++ src/app/common/hooks/useLoginForm.tsx
@@ -3,35 +3,39 @@ import { useForm, FormProvider } from "react-hook-form";
 import { UseFormReturn } from "react-hook-form/dist/types";
 
 const LoginFormContext: RFC = ({ children }) => {
-    const [formPage, setFormPage] = useState<FormPage>("login")
-    const formContext: LoginFormContext = { formPage, setFormPage, ...useForm<FormFields>(config) };
+  const [formPage, setFormPage] = useState<FormPage>("login");
+  const formContext: LoginFormContext = {
+    formPage,
+    setFormPage,
+    ...useForm<FormFields>(config)
+  };
 
-    return <FormProvider {...formContext}>{children}</FormProvider>;
+  return <FormProvider {...formContext}>{children}</FormProvider>;
 };
 
 export default LoginFormContext;
-export type { LoginFormContext, FormFields }
+export type { LoginFormContext, FormFields };
 
 const config: UseFormConfig = {
-    defaultValues: {
-        email: "",
-        password: "",
-        confirmPassword: ""
-    },
-    mode: 'onChange'
-}
+  defaultValues: {
+    email: "",
+    password: "",
+    confirmPassword: ""
+  },
+  mode: "onChange"
+};
 
-type FormPage = "login" | "forgot-password" | "reset-password"
-type FormPageSetter = Dispatch<SetStateAction<FormPage>>
-type LoginFormProps = { children: React.ReactNode }
-type RFC = React.FC<LoginFormProps>
-type UseFormConfig = Parameters<typeof useForm<FormFields>>[0]
+type FormPage = "login" | "forgot-password" | "reset-password";
+type FormPageSetter = Dispatch<SetStateAction<FormPage>>;
+type LoginFormProps = { children: React.ReactNode };
+type RFC = React.FC<LoginFormProps>;
+type UseFormConfig = Parameters<typeof useForm<FormFields>>[0];
 type FormFields = {
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 type LoginFormContext = {
-    formPage: FormPage
-    setFormPage: FormPageSetter
-} & UseFormReturn<FormFields>
+  formPage: FormPage;
+  setFormPage: FormPageSetter;
+} & UseFormReturn<FormFields>;
