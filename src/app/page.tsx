f49244c1c0fa5728f10ae9bb19541f96a316cd3d
eedris <alabialade@gmail.com>
2025-06-15 17:06:06 +0100

    feat: add Foreign Donations Banner component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import WhyCrowdr from "./home/home-components/WhyCrowdr";
 import Happening from "./home/home-components/Happening";
 import Partners from "./home/home-components/Partners";
 import { Mixpanel } from "@/utils/mixpanel";
+import ForeignDonationsBanner from "./home/home-components/ForeignDonationsBanner";
 
 export default function Home() {
   useEffect(() => {
@@ -19,24 +20,7 @@ export default function Home() {
     <main>
       <Navigation />
       <main className="font-satoshi">
-        <div className="bg-[#00CB6E69] text-xl py-4 flex flex-col md:flex-row items-center justify-center border-t border-b border-[#000000] px-3">
-          <p className="text-[#000000] text-center font-bold">
-            🎉 Now accepting international donations in all major currencies.{" "}
-          </p>
-          <div className="flex flex-row items-center justify-center">
-            <span
-              className="text-[#079455] text-xl font-normal italic underline cursor-pointer ml-1"
-              onClick={() => {
-                window.open(
-                  "https://blog.oncrowdr.com/how-to-raise-money-from-international-donations-for-your-fundraiser-in-nigeria/",
-                  "_blank"
-                );
-              }}>
-              {" "}
-              Powered by Apple Pay
-            </span>
-          </div>
-        </div>
+        <ForeignDonationsBanner />
         <Header />
         <Todo />
         <WhyCrowdr />
