c712dc2af84874f5885fa8ee8342f9038d5f2686
Daniel Arikawe <arikawedaniel@gmail.com>
2025-06-22 08:23:39 +0100

    feat: updates

--- app/(protected)/admin/common/services/campaign/index.ts
+++ app/(protected)/admin/common/services/campaign/index.ts
@@ -1,27 +1,30 @@
-import axios from "axios"
-import { IGetCampaignsParams, IGetCampaignsResponse } from "./models/GetCampaigns"
-import { IGetCampaignStatsResponse } from "./models/GetCampaignStats"
+import axios from "axios";
+import {
+  IGetCampaignsParams,
+  IGetCampaignsResponse
+} from "./models/GetCampaigns";
+import { IGetCampaignStatsResponse } from "./models/GetCampaignStats";
 
 const getCampaigns = async (params: Partial<IGetCampaignsParams> = {}) => {
-  const url = `/admin/campaigns`
+  const url = `/admin/campaigns`;
 
   try {
-    const res = await axios.get<IGetCampaignsResponse>(url, {params})
-    return res.data.data
+    const res = await axios.get<IGetCampaignsResponse>(url, { params });
+    return res.data.data;
   } catch (error: any) {
-    throw new Error(error.response?.data?.message || "An error occurred")
+    throw new Error(error.response?.data?.message || "An error occurred");
   }
-}
+};
 
 const getCampaignStats = async () => {
-  const url = `/admin/campaigns-stats`
+  const url = `/admin/campaigns-stats`;
 
   try {
-    const res = await axios.get<IGetCampaignStatsResponse>(url)
-    return res.data.data
+    const res = await axios.get<IGetCampaignStatsResponse>(url);
+    return res.data.data;
   } catch (error: any) {
-    throw new Error(error.response?.data?.message || "An error occurred")
+    throw new Error(error.response?.data?.message || "An error occurred");
   }
-}
+};
 
-export default { getCampaigns, getCampaignStats }
+export default { getCampaigns, getCampaignStats };
